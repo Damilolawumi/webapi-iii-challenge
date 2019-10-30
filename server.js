beef6a98a95dd3a5489c8abd7f4cf27f866580e0
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-30 22:57:19 +0100

    created a function that validates the user id on every request that expects a user id parameter

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
-const postRouter = require('./posts/postDb');
+// const postRouter = require('./posts/postDb');
+const userRouter = require('./users/userRouter');
 
 const server = express();
 
@@ -12,6 +13,9 @@ server.use((req, res, next) => {
   console.log('Yay it worked!!')
   next();
 })
+
+server.use('/api/users', logger, userRouter);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
@@ -19,7 +23,12 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+  next();
 };
 
 module.exports = server;
